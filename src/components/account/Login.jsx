14e04120ee0886dc69881d8bd19ee869b0a71b65
Anushka Chaudhary <anushkachaudhary048@gmail.com>
2025-01-24 15:15:27 +0530

    first commit

---
+++ src/components/account/Login.jsx
@@ -0,0 +1,16 @@
+
+import { Box } from '@mui/material';
+
+
+const Login = () => {
+
+    const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
+
+    return (
+        <Box>
+            <img src={imageURL} alt="login" />
+        </Box>
+    )
+}
+
+export default Login;
